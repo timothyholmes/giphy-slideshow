9c289afbeac5b2119cbd616c569c8e3772093d7d
Tim Holmes <tholmes@starchase.com>
2016-03-11 16:11:10 -0500

    Finished setting up blank project

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var Promise = require('es6-promise').Promise;
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var del = require('del');
+var runSequence = require('run-sequence');
+
+gulp.task('sass', function(){
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('src/scss/**/*.scss', ['sass']); 
+});
+
+gulp.task('useref', function(){
+  return gulp.src('src/index.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify({mangle: false})))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist', 
+    'sass', 
+    'useref',
+    callback
+  )
+})
+
+gulp.task('default', function (callback) {
+  runSequence(['sass','watch'],
+    callback
+  )
+})
